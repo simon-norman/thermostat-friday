e2ca43ba2a6dc5dfd9c202456f13070fffff7452
Simon <01secn@gmail.com>
2019-03-08 14:37:56 +0000

    Can increase temperature using up button

--- src/thermostatController.js
+++ src/thermostatController.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   var thermostat = new Thermostat()
 
+  function updateTempInView() {
+    $('#temp-display').text(`${thermostat.temp}°C`)
+  }
+
   $('#arrow-up').on('click', function() {
-    thermostat.up()
+    thermostat.up(1)
+    updateTempInView()
   })
 })
